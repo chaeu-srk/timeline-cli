8a1d445e8f4b1136a23dc46b00d230d3fdfc5837
Kelvin Hsu <kelvin@Kelvins-MacBook-Air.local>
2025-08-12 22:55:21 +1000

    Added fs functions that get relavent paths

--- src/filesys.cpp
+++ src/filesys.cpp
@@ -1,11 +1,14 @@
 #include "filesys.h"
+#include <filesystem>
+#include <fstream>
+#include <stdexcept>
 
 namespace filesys {
 
-fs::path get_save_path() {
+std::filesystem::path get_save_path() {
     const char* xdg_data_home = std::getenv("XDG_DATA_HOME");
 
-    fs::path base_path;
+    std::filesystem::path base_path;
     // Checking if null ptr and if xdg_data_home exists
     if (xdg_data_home && *xdg_data_home) {
         base_path = xdg_data_home;
@@ -14,13 +17,44 @@ fs::path get_save_path() {
         if (!home) {
             throw std::runtime_error("HOME environment var not set");
         }
-        base_path = fs::path(home) / ".local" / "share";
+        base_path = std::filesystem::path(home) / ".local" / "share";
     }
 
-    fs::path app_path = base_path / "timeline";
+    std::filesystem::path app_path = base_path / "timeline";
 
-    fs::create_directory(app_path);
+    std::filesystem::create_directory(app_path);
     return app_path;
+}
+
+std::filesystem::path create_new_project_save(const std::string& project_name) {
+    std::filesystem::path save_path = get_save_path() / project_name;
+
+    // check if it exists
+    if (std::filesystem::exists(save_path)) {
+        throw std::invalid_argument("project already exists");
+    }
+
+    return save_path;
+}
+
+std::filesystem::path get_cached_project_path() {
+    const std::filesystem::path save_path = get_save_path();
+    std::ifstream cache_file{ save_path / CACHE_FILE_NAME };
+
+    if (!cache_file) {
+        throw std::invalid_argument("Cache file error: " + save_path.string() + '\n');
+    }
+
+    std::string proj_name;
+    cache_file >> proj_name;
+
+    return save_path / proj_name;
+}
+
+void cache_project_path(const std::string& project_name) {
+    const std::filesystem::path save_path = get_save_path();
+    std::ofstream cache_file{ save_path / CACHE_FILE_NAME };
+    cache_file << project_name;
 }
 
 } // namespace filesys
--- src/filesys.h
+++ src/filesys.h
@@ -4,11 +4,30 @@
 #include <filesystem>
 
 namespace filesys {
-namespace fs = std::filesystem;
+
+inline constexpr std::string CACHE_FILE_NAME{ "cache.txt" };
 
 /**
- * @brief returns the save path that stores timeline saves
+ * @brief gets the save path used by the program
  */
-fs::path get_save_path();
+std::filesystem::path get_save_path();
+
+/**
+ * All it does is check if the project already exists,
+ * File creation can just be delagated to ofstream
+ */
+std::filesystem::path create_new_project_save(const std::string& project_name);
+
+/**
+ * @brief gets the path of last opened project
+ *
+ * @throws invalid argument if cache file doesnt exist
+ */
+std::filesystem::path get_cached_project_path();
+
+/**
+ * @brief saves the project passed into the cache file
+ */
+void cache_project_path(const std::string& project_name);
 
 } // namespace filesys
